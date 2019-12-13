f87a13408e66e097fc148128a83ba7f617e7dac9
lemengbin <lemengbin@163.com>
2019-12-13 18:22:55 +0800

    统计文件中非空白行和非注释代码的其他行总数

--- line_calc.sh
+++ line_calc.sh
@@ -5,6 +5,19 @@ if [ $# != 1 ]; then
     exit -1;
 fi
 
-echo -n "file: $1, line number: "
-line_no=`grep -v -e '^[[:space:]]*$' $1 | grep -v -e '^{[[:space:]]*$' | grep -v -e '^[[:space:]]*}$' | wc -l`
+file="$1"
+echo -n "file: $file, valid line number: "
+
+# 1. filter comments /* ... */
+sed -r ':a; s%(.*)/\*.*\*/%\1%; ta; /\/\*/ !b; N; ba' "$file" > "$file.tmp.1"
+
+# 2. filter comment //
+grep -v -e '^[[:space:]]*//' "$file.tmp.1" > "$file.tmp.2"
+
+# 3. filter blank line
+#line_no=`grep -v -e '^[[:space:]]*$' $1 | grep -v -e '^[[:space:]]*{[[:space:]]*$' | grep -v -e '^[[:space:]]*}[[:space:]]*$' | wc -l`
+line_no=`grep -v -e '^[[:space:]]*$' "$file.tmp.2" | wc -l`
 echo $line_no
+
+# 4. delete temporaray files
+rm "$file.tmp.1" "$file.tmp.2" -rf
