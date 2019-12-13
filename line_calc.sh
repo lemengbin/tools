ec1d647df0f5782f36ac980c949db0c162833320
lemengbin <lemengbin@163.com>
2019-12-13 18:23:53 +0800

    支持统计目录或文件的非空白行和非注释行的总数

--- line_calc.sh
+++ line_calc.sh
@@ -1,23 +1,71 @@
-#!/bin/sh
+#!/bin/bash
+
+CalcFromFile()
+{
+    file="$1";
+
+    # 0. filter non-cpp files
+    suffix=${file##*.};
+    if [ $suffix != 'cpp' ] && [ $suffix != 'h' ]; then
+        return;
+    fi
+
+    # 1. filter comments /* ... */
+    sed -r ':a; s%(.*)/\*.*\*/%\1%; ta; /\/\*/ !b; N; ba' "$file" > "$file.tmp.1"
+
+    # 2. filter comment //
+    grep -v -e '^[[:space:]]*//' "$file.tmp.1" > "$file.tmp.2"
+
+    # 3. filter blank line
+    #line=`grep -v -e '^[[:space:]]*$' $1 | grep -v -e '^[[:space:]]*{[[:space:]]*$' | grep -v -e '^[[:space:]]*}[[:space:]]*$' | wc -l`
+    line=`grep -v -e '^[[:space:]]*$' "$file.tmp.2" | wc -l`
+
+    # 4. delete temporaray files
+    rm "$file.tmp.1" "$file.tmp.2" -rf
+
+    echo "file: $file, valid line number: $line"
+    all_lines=$(($all_lines+$line))
+}
+
+CalcFromDir()
+{
+    for item in `ls $1`
+    do
+        child="$1/$item"
+        if [ -f "$child" ]; then
+            CalcFromFile "$child"
+        else
+            CalcFromDir $child
+        fi
+    done
+}
+
+main()
+{
+    file="$1"
+
+    if [ -f "$file" ]; then
+        CalcFromFile "$file"
+    elif [ -d "$file" ]; then
+        CalcFromDir "$file"
+    else
+        echo "Please input a valid file or directory"
+        exit -2
+    fi
+
+    echo "--------------------------------------------------"
+    echo "all valid line number: $all_lines"
+    echo "--------------------------------------------------"
+    echo;
+}
+
+###############################################################
 
 if [ $# != 1 ]; then
-    echo "Please input your file"
+    echo "Please input your file or directory";
     exit -1;
 fi
 
-file="$1"
-echo -n "file: $file, valid line number: "
+all_lines=0;
 
-# 1. filter comments /* ... */
-sed -r ':a; s%(.*)/\*.*\*/%\1%; ta; /\/\*/ !b; N; ba' "$file" > "$file.tmp.1"
-
-# 2. filter comment //
-grep -v -e '^[[:space:]]*//' "$file.tmp.1" > "$file.tmp.2"
-
-# 3. filter blank line
-#line_no=`grep -v -e '^[[:space:]]*$' $1 | grep -v -e '^[[:space:]]*{[[:space:]]*$' | grep -v -e '^[[:space:]]*}[[:space:]]*$' | wc -l`
-line_no=`grep -v -e '^[[:space:]]*$' "$file.tmp.2" | wc -l`
-echo $line_no
-
-# 4. delete temporaray files
-rm "$file.tmp.1" "$file.tmp.2" -rf
+main $1
